9c47e43d320d9720fc1c0cababf190f92e26beab
Paul Bagyenda <bagyenda@dsmagic.com>
2024-10-24 08:44:49 +0300

    - renamed berbmpstring class

--- berbmpstring.go
+++ berbmpstring.go
@@ -4,23 +4,23 @@ import (
 	"io"
 )
 
-type BerBmpString struct {
+type BerBMPString struct {
 	BerOctetString
 }
 
 var bmpstringTag = NewBerTag(UNIVERSAL_CLASS, PRIMITIVE, BMP_STRING_TAG)
 
-func NewBerBmpString(value []byte) *BerBmpString {
-	return &BerBmpString{BerOctetString: *NewBerOctetString(value)}
+func NewBerBmpString(value []byte) *BerBMPString {
+	return &BerBMPString{BerOctetString: *NewBerOctetString(value)}
 }
 
-func (b *BerBmpString) Encode(reversedWriter io.Writer, withTagList ...bool) (int, error) {
+func (b *BerBMPString) Encode(reversedWriter io.Writer, withTagList ...bool) (int, error) {
 	return b.EncodeUsingTag(bmpstringTag, reversedWriter, withTagList...)
 }
 
-func (b *BerBmpString) Decode(input io.Reader, withTagList ...bool) (int, error) {
+func (b *BerBMPString) Decode(input io.Reader, withTagList ...bool) (int, error) {
 	return b.DecodeUsingTag(bmpstringTag, input, withTagList...)
 }
-func (b *BerBmpString) GetTag() *BerTag {
+func (b *BerBMPString) GetTag() *BerTag {
 	return bmpstringTag
 }
